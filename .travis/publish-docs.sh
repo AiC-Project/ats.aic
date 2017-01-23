4d6a32fde870ff62405c32cd20ab523cb0baba80
Marco Mariani <marco.mariani@alterway.fr>
2017-01-23 13:37:26 +0100

    moved travis stuff to .travis

--- .travis/publish-docs.sh
+++ .travis/publish-docs.sh
@@ -6,7 +6,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in doc/scripts/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in .travis/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval "$(ssh-agent -s)"
 ssh-add deploy_key
@@ -15,14 +15,22 @@ ssh-add deploy_key
 ssh-add -l
 git config --global user.email "Travis Docs CI"
 git config --global user.name "Travis Docs CI"
-git clone "$GIT_PUB_REPO" "$GIT_PUB_LOCAL_DIR" -b "${GIT_PUB_BRANCH}"
+git clone "$GIT_PUB_REPO" "$GIT_PUB_LOCAL_DIR" -b "$GIT_PUB_BRANCH"
 
 # remove old version
-git --git-dir="$GIT_PUB_LOCAL_DIR" rm -r "$GIT_PUB_SUB_DIR"
+cd "$GIT_PUB_LOCAL_DIR"
+git rm -r "$GIT_PUB_SUB_DIR"
+cd ..
 
 # add new version
-cp -r "${GIT_PUB_BUILD_DIR}" "${GIT_PUB_LOCAL_DIR}${GIT_PUB_SUB_DIR}"
+mv "$GIT_PUB_BUILD_DIR" "${GIT_PUB_LOCAL_DIR:?}/${GIT_PUB_SUB_DIR:?}"
+
+# publish
 cd "$GIT_PUB_LOCAL_DIR"
-git add .
+git add "$GIT_PUB_SUB_DIR"
+
+# nothing to commit?
+git diff --staged --quiet && exit 0
+
 git commit -m "Updating docs."
 git push "$GIT_PUB_REPO" "$GIT_PUB_BRANCH"
